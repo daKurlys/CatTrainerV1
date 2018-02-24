146d9408cafef17524fb3cc11cd799998ae787e5
daKurlys <36773591+daKurlys@users.noreply.github.com>
2018-02-23 19:20:57 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1519431562,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/sofa-sheet0.png",
+		"images/cat-sheet0.png",
+		"images/squirt-sheet0.png",
+		"images/cattree-sheet0.png",
+		"images/sofa2-sheet0.png",
+		"images/sofa3-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
